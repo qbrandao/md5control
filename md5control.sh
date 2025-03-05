7e0edbb1283c9dd816601a3406ef04fab82fd22a
Quentin Brandao <q.brandao1@gmail.com>
2025-03-05 17:15:11 +0100

    fix space between if

--- md5control.sh
+++ md5control.sh
@@ -10,15 +10,15 @@ if [ "$#" -lt 2 ]; then
     exit 1
 fi
 
-if ["$1" == "test"]; then
-    if [$2 == true]; then
-        if [$2[0] == "."]; then #extention
+if [ "$1" == "test" ]; then
+    if [ $2 == true ]; then
+        if [ $2[0] == "." ]; then #extention
             for f in *$2; do
                 md5raw_new=$(md5sum $f.md5)
                 md5_new=(${md5raw_new//\t/ })
                 md5raw_old=$(more $f.md5)
                 md5_old=(${md5raw_old//\t/ })
-                if [md5_new[0] == md5_old[0]]; then
+                if [ md5_new[0] == md5_old[0] ]; then
                     echo -e "$f is OK!"
                 else
                     echo -e "$f ERROR MD5 KEY IS NOT THE SAME!"
@@ -29,7 +29,7 @@ if ["$1" == "test"]; then
             md5_new=(${md5raw_new//\t/ })
             md5raw_old=$(more $2.md5)
             md5_old=(${md5raw_old//\t/ })
-            if [md5_new[0] == md5_old[0]]; then
+            if [ md5_new[0] == md5_old[0] ]; then
                 echo -e "$2 is OK!"
             else
                 echo -e "$2 ERROR MD5 KEY IS NOT THE SAME!"
@@ -40,9 +40,9 @@ if ["$1" == "test"]; then
         echo -e $usage $sub $ex
         exit 1
     fi
-elif ["$1" == "create"]; then
-    if [$2 == true]; then
-        if [$2[0] == "."]; then
+elif [ "$1" == "create" ]; then
+    if [ $2 == true ]; then
+        if [ $2[0] == "." ]; then
             for f in *$2; do
                 md5sum $f > $f.md5
             done
