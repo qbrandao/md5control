700403f5fdabfcf1ecc2b160aa74fb5fca812f8d
Quentin Brandao <q.brandao1@gmail.com>
2025-03-06 17:30:17 +0100

    Merge branch 'main' into dev

--- md5control.sh
+++ md5control.sh
@@ -15,9 +15,9 @@ if [ "$1" == "test" ]; then
         if [ "$2" == .* ]; then #extention
             for f in *"$2"; do
                 if [[ -f "$f.md5" ]]; then
-                    md5raw_new=$(md5sum $f.md5)
+                    md5raw_new=$(md5sum $f)
                     md5_new=(${md5raw_new//\t/ })
-                    md5raw_old=$(more $f.md5)
+                    md5raw_old=$(cat $f.md5)
                     md5_old=(${md5raw_old//\t/ })
 
                     if [[ "${md5_new[0]}" == "${md5_old[0]}" ]]; then
@@ -31,11 +31,11 @@ if [ "$1" == "test" ]; then
             done
         else # filename
             if [[ -f "$2.md5" ]]; then
-                md5raw_new=$(md5sum $2.md5)
+                md5raw_new=$(md5sum $2)
                 md5_new=(${md5raw_new//\t/ })
-                md5raw_old=$(more $2.md5)
+                md5raw_old=$(cat $2.md5)
                 md5_old=(${md5raw_old//\t/ })
-                if [ md5_new[0] == md5_old[0] ]; then
+                if [[ md5_new[0] == md5_old[0] ]]; then
                     echo -e "$2 is OK!"
                 else
                     echo -e "$2 ERROR MD5 KEY IS NOT THE SAME!"
@@ -52,7 +52,7 @@ if [ "$1" == "test" ]; then
 
 elif [ "$1" == "create" ]; then
     if [ -n "$2" ]; then
-        for f in "*$2"; do
+        for f in *"$2"; do
             if [ -f "$f" ]; then
                 md5sum "$f" > "$f.md5"
                 echo -e "MD5 checksum created for $f"
